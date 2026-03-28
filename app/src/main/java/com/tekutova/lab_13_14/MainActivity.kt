b2f88d8b18c9cb64c4e000937315d13f8eb2edca
Никита <dunyushkin2007@mail.ru>
2026-03-28 18:07:57 +0300

    Step 5: Connect ViewModel to UI

--- app/src/main/java/com/tekutova/lab_13_14/MainActivity.kt
+++ app/src/main/java/com/tekutova/lab_13_14/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.tekutova.lab_13_14.ui.theme.Lab_1314Theme
+import com.tekutova.lab_13_14.ui_model.GameScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab_1314Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPaddding ->
+                    GameScreen(
+                        modifier = Modifier.padding(innerPaddding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab_1314Theme {
-        Greeting("Android")
     }
 }
